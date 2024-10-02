e10539eca74501969749c6bb5b1bd72e522470c7
BelminMusic <150080851+BelminMusic@users.noreply.github.com>
2024-10-02 17:57:24 +0200

    football inserted

--- .github/script.js
+++ .github/script.js
@@ -1,5 +1,6 @@
 function preload() {
   LevelsBackground = loadImage("img/GoalDash.png");
+  TheBall = loadImage("img/football.png");
 }
 
 function setup() {
@@ -9,4 +10,5 @@ function setup() {
 function draw() {
   // Display the background image on the canvas
   image(LevelsBackground, 0, 0, 800, 500);
+  image(TheBall, 400, 400, 40, 40);
 }
