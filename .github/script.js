806b3563df01e35db9a45988eb20c42a646ed296
BelminMusic <150080851+BelminMusic@users.noreply.github.com>
2024-10-03 12:56:17 +0200

    ball movment + connceting to key

--- .github/script.js
+++ .github/script.js
@@ -1,3 +1,10 @@
+
+
+let LevelsBackground;
+let TheBall;
+let ballX = 400;
+let ballY = 400;
+
 function preload() {
   LevelsBackground = loadImage("img/GoalDash.png");
   TheBall = loadImage("img/football.png");
@@ -10,5 +17,13 @@ function setup() {
 function draw() {
   // Display the background image on the canvas
   image(LevelsBackground, 0, 0, 800, 500);
-  image(TheBall, 400, 400, 40, 40);
+  image(TheBall, ballX, ballY, 40, 40);
+}
+
+function keyPressed() {
+  // Check if the "Enter" key is pressed
+  if (keyCode === ENTER) {
+    // Move the ball upwards
+    ballY -= 10;
+  }
 }
